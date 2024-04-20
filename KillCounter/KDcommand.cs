24cfb21ecfcee967f31bfecd193d8ab62c624e6f
waltuh <162900253+Waltuhs@users.noreply.github.com>
2024-04-20 19:29:59 +0100

    Patch for dnt

--- KillCounter/KDcommand.cs
+++ KillCounter/KDcommand.cs
@@ -16,24 +16,31 @@ namespace KillCounter
         public string Description { get; } = "Gets the player's Kill/Death ratio from the server-side database";
         public bool Execute(ArraySegment<string> arguments, ICommandSender sender, out string response)
         {
-
             Player player = Player.Get(sender);
-            using (var db = new LiteDatabase("kill_counter.db"))
+
+            if (!player.DoNotTrack)
             {
-                var killCollection = db.GetCollection<KillCount>("kill_counts");
-                var deathCollection = db.GetCollection<DeathCount>("death_counts");
+                using (var db = new LiteDatabase("kill_counter.db"))
+                {
+                    var killCollection = db.GetCollection<KillCount>("kill_counts");
+                    var deathCollection = db.GetCollection<DeathCount>("death_counts");
 
-                // get kill count spaghetti
-                var killCount = killCollection.FindOne(Query.EQ("PlayerId", player.UserId));
-                int kills = killCount != null ? killCount.Kills : 0;
+                    // get kill count spaghetti
+                    var killCount = killCollection.FindOne(Query.EQ("PlayerId", player.UserId));
+                    int kills = killCount != null ? killCount.Kills : 0;
 
-                // get deaths
-                var deathCount = deathCollection.FindOne(Query.EQ("PlayerId", player.UserId));
-                int deaths = deathCount != null ? deathCount.Deaths : 0;
+                    // get deaths
+                    var deathCount = deathCollection.FindOne(Query.EQ("PlayerId", player.UserId));
+                    int deaths = deathCount != null ? deathCount.Deaths : 0;
 
-                // calc + print
-                double ratio = deaths != 0 ? (double)kills / deaths : kills;
-                response = $"\nPlayer: {player.Nickname}\nKills: {kills}\nDeaths: {deaths}\nKill/Death Ratio: {ratio:F2}";
+                    // calc + print
+                    double ratio = deaths != 0 ? (double)kills / deaths : kills;
+                    response = $"\nPlayer: {player.Nickname}\nKills: {kills}\nDeaths: {deaths}\nKill/Death Ratio: {ratio:F2}";
+                }
+            }
+            else
+            {
+                response = "you have dnt active.";
             }
             return true;
         }
